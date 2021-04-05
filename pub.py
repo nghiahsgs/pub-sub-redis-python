c47357fa358dbe0ed0c20f88f124e18b0502677f
nghiahsgs <nghiahsgs@gmail.com>
2021-04-05 14:35:35 +0700

    Update pub.py

--- pub.py
+++ pub.py
@@ -1,6 +1,6 @@
 import redis
 import time
-r = redis.Redis(host='127.0.0.1',port = 6379)
+r = redis.Redis(host='127.0.0.1',port = 6379,charset='utf-8',decode_responses=True)
 dem = 0
 while True:
   p = r.publish(channel ='channel3',message="nghiahsgs%s"%dem)
